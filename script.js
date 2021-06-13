b5746b1e913f9f7be1e190d786938832b95b0c62
Rohanlachman <113342@emmauscollege.nl>
2021-06-13 17:20:19 +0000

    enalaatste verandering
    Signed-off-by: Rohanlachman <113342@emmauscollege.nl>

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/// @ts-check
+  /// @ts-check
 /// <reference path=".gitpod/p5.global-mode.d.ts" />
 "use strict";
 
@@ -23,13 +23,13 @@ const GAMEOVER = 2;
 var spelStatus = SPELEN;
 
 var spelerX = 0; // x-positie van speler
-var spelerY = 0; // y-positie van speler
+var Speler1Y = 300; // y-positie van speler
 
-var kogelX = 0;    // x-positie van kogel
-var kogelY = 0;    // y-positie van kogel
+var KogelX = 650;    // x-positie van kogel
+var KogelY = 350;    // y-positie van kogel
 
 var vijandX = 0;   // x-positie van vijand
-var vijandY = 0;   // y-positie van vijand
+var Speler2Y = 300;   // y-positie van vijand
 
 var score = 0; // aantal behaalde punten
 
@@ -42,47 +42,41 @@ var score = 0; // aantal behaalde punten
 /* ********************************************* */
 
 
-/**
- * Tekent het speelveld
- */
-var tekenVeld = function () {
-  fill("white");
-  rect(20, 20, width - 2 * 20, height - 2 * 20);
-};
+draw = function() {
 
+background('blue');
 
-/**
- * Tekent de vijand
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenVijand = function(x, y) {
-    fill("green");
-        rect(1150, 300, 20, 100);
+    fill("white");
+    rect(20, 20, width - 2 * 20, height - 2 * 20);
 
-};
+        fill("green");
+        rect(1150, Speler2Y, 20, 100);
 
+if (keyIsDown(UP_ARROW)) {
+    Speler2Y -= 5
+  }
+if (keyIsDown(DOWN_ARROW)) {
+    Speler2Y += 5
+ }
 
-/**
- * Tekent de kogel of de bal
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenKogel = function(x, y) {
-fill("orange");
-  ellipse(650, 350, 50, 50);
+        fill("orange");
+  ellipse(KogelX, KogelY, 50, 50);
 
-};
+KogelX += 5
+if (KogelY === Speler2Y) {KogelX -=5};
 
+    
+    fill("purple");
+    rect(100, Speler1Y, 20, 100);
 
-/**
- * Tekent de speler
- * @param {number} x x-coördinaat
- * @param {number} y y-coördinaat
- */
-var tekenSpeler1 = function(x, y) {
- fill("purple");
-    rect(100, 300, 20, 100);
+if (keyIsDown(87)) {
+    Speler1Y -= 5
+  }
+
+if (keyIsDown(83)) {
+    Speler1Y += 5
+  }  
+
 
 };
 
@@ -149,7 +143,7 @@ function setup() {
   createCanvas(1280, 720);
 
   // Kleur de achtergrond blauw, zodat je het kunt zien
-  background('blue');
+  
 }
 
 
@@ -186,3 +180,4 @@ function draw() {
       break;
   }
 }
+
